b50b0b74c0f7f6f2e313fa412324fea2e709cb94
Cotelea <marius.cotelea1003@gmail.com>
2021-04-01 12:30:50 +0300

    am facut site-ul responsive

--- scripts/script2.js
+++ scripts/script2.js
@@ -4,18 +4,17 @@ if(thisMonth<10){
     thisMonth = '0' + thisMonth;
 };
 var thisYear = new Date().getFullYear();
-var thisHour = new Date().getHours();
-var thisMinute = new Date().getMinutes();
-var presentDate = `${thisDay}-${thisMonth}-${thisYear} ${thisHour}:${thisMinute}`;
+var presentDate = `${thisYear}-${thisMonth}-${thisDay}`;
 window.addEventListener('load', (event) => {
     for(var i=1; i<=18;i++){
         var buttonDates = document.getElementById('option'+i).value;
-        if (i !== i) {
-            continue;
-        }
         if(buttonDates < presentDate){
             document.getElementById('option'+i).setAttribute('disabled','');
-        }
-    }
+        };
+    };
 });
- 
+
+if(document.getElementById("option18").checked()){
+    document.getElementById('buton-final').removeAttribute('disabled');
+    console.log('Success');
+};
