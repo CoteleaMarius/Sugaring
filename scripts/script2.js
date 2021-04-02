daded8e6dfb479be3b0b34eebeb29c74ff1321f3
Cotelea <marius.cotelea1003@gmail.com>
2021-04-02 21:04:37 +0300

    am adaugat scriptul pentru activarea butonului si am facut unele elemente sa fie responsive

--- scripts/script2.js
+++ scripts/script2.js
@@ -1,20 +1,8 @@
-var thisDay = new Date().getDate();
-var thisMonth = new Date().getMonth() + 1;
-if(thisMonth<10){
-    thisMonth = '0' + thisMonth;
-};
-var thisYear = new Date().getFullYear();
-var presentDate = `${thisYear}-${thisMonth}-${thisDay}`;
-window.addEventListener('load', (event) => {
-    for(var i=1; i<=18;i++){
-        var buttonDates = document.getElementById('option'+i).value;
-        if(buttonDates < presentDate){
-            document.getElementById('option'+i).setAttribute('disabled','');
-        };
-    };
-});
-
-if(document.getElementById("option18").checked()){
-    document.getElementById('buton-final').removeAttribute('disabled');
-    console.log('Success');
-};
+$(document).ready(function () {
+    var $submit = $("#buton-final").prop('disabled', true);
+      $cbs = $('input[name="options"]').click(function () {
+        if ($cbs.is(":checked")) {
+            $submit.removeAttr('disabled');
+        }
+      });
+  });
