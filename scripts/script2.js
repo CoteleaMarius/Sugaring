0938ae9cd8bdd3cb72f34dfe8666c188e118f3c5
Cotelea <marius.cotelea1003@gmail.com>
2021-04-04 11:36:09 +0300

    am adaugat scriptul pentru dezactivarea butonului la programare

--- scripts/script2.js
+++ scripts/script2.js
@@ -1,8 +1,20 @@
 $(document).ready(function () {
-    var $submit = $("#buton-final").prop('disabled', true);
-      $cbs = $('input[name="options"]').click(function () {
-        if ($cbs.is(":checked")) {
-            $submit.removeAttr('disabled');
-        }
-      });
+  var $submit = $("#buton-final").prop('disabled', true);
+  $cbs = $('input[name="options"]').click(function () {
+    if ($cbs.is(":checked")) {
+      $submit.removeAttr('disabled');
+    }
   });
+});
+
+function test(clickedId) {
+  let thisDay = new Date().getDate();
+  let thisMonth = new Date().getMonth() + 1;
+  let thisYear = new Date().getFullYear();
+  let thisHour = new Date().getHours();
+  let thisMinute = new Date().getMinutes();
+  if (document.getElementById(clickedId).value < thisDay || document.getElementById(clickedId).title < thisMonth || document.getElementById(clickedId).size < thisYear || document.getElementById(clickedId).slot < thisHour || document.getElementById(clickedId).lang < thisMinute){
+    document.getElementById(clickedId).setAttribute('disabled', '');
+    document.getElementById(clickedId).checked = false;
+  };
+}
